2e949bad7904346f7d11abb0c306dc26644fc810
Pawel Szmit <pszmit@valuelogic.one>
2019-02-23 09:48:00 +0100

    Add project files.

---
+++ NetCoreAppSettings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreAppSettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
